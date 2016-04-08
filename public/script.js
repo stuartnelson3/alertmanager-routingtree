370eea7c87ec4b023442817308b64a27e7c94d44
stuart nelson <stuartnelson3@gmail.com>
2016-04-08 17:07:59 +0200

    highlight matched circle

--- public/script.js
+++ public/script.js
@@ -20,11 +20,78 @@ var svg = d3.select("body").append("svg")
  .append("g")
  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
- d3.json("config.json", function(error, graph) {
-   if (error) throw error;
+d3.select(".js-find-match").on("click", function() {
+  var searchValue = document.querySelector("input").value
+  // this needs to be an object of key-value pairs
+  labelSet = {"service":"files", "severity":"critical"};
+  var matches = match(root, labelSet)
+  var nodes = tree.nodes(root);
+  var idx = nodes.map(function(n) { return n.id }).indexOf(matches[0].id)
+  nodes[idx].matched = true;
+  update(root);
+  // highlight the node that matches, maybe animate way through if possible
+});
+
+// Match does a depth-first left-to-right search through the route tree
+// and returns the matching routing nodes.
+function match(root, labelSet) {
+  // See if the node is a match. If it is, recurse through the children.
+	if (!matchLabels(root.matchers, labelSet)) {
+		return [];
+	}
+
+
+  var all = []
+
+  if (root.children) {
+    for (var j = 0; j < root.children.length; j++) {
+      child = root.children[j];
+      matches = match(child, labelSet)
+
+      all = all.concat(matches);
+
+      if (matches && !child.Continue) {
+        break;
+      }
+    }
+  }
+
+  // If no child nodes were matches, the current node itself is a match.
+	if (all.length === 0) {
+		all.push(root);
+	}
+
+	return all
+}
+
+function matchLabels(matchers, labelSet) {
+  for (var j = 0; j < matchers.length; j++) {
+    if (!matchLabel(matchers[j], labelSet)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function matchLabel(matcher, labelSet) {
+  // TODO: need to parse labelset into k:v pairs
+  var v = labelSet[matcher.name];
+
+  if (matcher.isRegex) {
+    return matcher.value.test(v)
+  }
+
+  return matcher.value === v;
+}
+
+d3.json("config.json", function(error, graph) {
+  //TODO: Current MarshalJSON is returning an {} for regex, get it to return a
+  //stringified form.
+  if (error) throw error;
 
   root = graph.data.Route;
 
+  root.parent = null;
   massage(root)
 
   update(root);
@@ -33,12 +100,36 @@ var svg = d3.select("body").append("svg")
 function massage(root) {
   if (!root) return;
 
-  root.parent = null;
   root.children = root.Routes
+
+  // add match and matchregex as objects, set matchregex.isRegex = true
+  var matchers = []
+  if (root.Match) {
+    for (var key in root.Match) {
+      var o = {};
+      o.isRegex = false;
+      o.value = root.Match[key];
+      o.name = key;
+      matchers.push(o);
+    }
+  }
+
+  if (root.MatchRE) {
+    for (var key in root.MatchRE) {
+      var o = {};
+      o.isRegex = true;
+      o.value = new RegExp(root.MatchRE[key]);
+      o.name = key;
+      matchers.push(o);
+    }
+  }
+
+  root.matchers = matchers;
 
   if (!root.children) return;
 
   root.children.forEach(function(child) {
+    child.parent = root;
     massage(child)
   });
 }
@@ -61,11 +152,15 @@ function update(root) {
   var nodeEnter = node.enter().append("g")
    .attr("class", "node")
    .attr("transform", function(d) {
-    return "translate(" + d.y + "," + d.x + ")"; });
+    return "translate(" + d.y + "," + d.x + ")";
+   });
 
   nodeEnter.append("circle")
-   .attr("r", 10)
-   .style("fill", "steelblue");
+   .attr("r", 10);
+
+   node.select("circle").style("fill", function(d) {
+     return d.matched ? "yellow" : "steelblue";
+   });
 
   nodeEnter.append("text")
    .attr("x", function(d) {
